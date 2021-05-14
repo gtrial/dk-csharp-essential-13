8c5aa8c712022bdb03c875c0e684ddc6aad09106
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-14 12:19:10 +0300

    Initial commit

---
+++ task02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task02
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            //Расширьте задание 2, так, чтобы в одном столбце одновременно могло быть две цепочки символов
+        }
+    }
+}
